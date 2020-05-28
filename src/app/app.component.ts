80d6b5fd3bb40fea6bafd3ca47de5268303a665a
pavithrasekar24 <spavithracse8@gmail.com>
2020-05-28 19:17:20 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,48 @@
 import { Component, VERSION } from "@angular/core";
-import {DataService} from "./service/data.service";
-@Component({
+import { DataService } from "./service/data.service";
+@Component({ 
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name:any = "Angular " + VERSION.major;
-  className="test";
+  name: any = "Angular " + VERSION.major;
+  className = "test<script></script>";
   test = {
     name: "Testing my string"
-    };
-  details={
-      name:'Pavithra',age:23
-    };
-  constructor(public dataService:DataService){
-
+  };
+  details = {
+    name: "Pavithra",
+    age: 23
+  };
+  user = null;
+  user1 = [
+    { name: "abc", age: "23" },
+    { name: "xyz", age: "24" },
+    { name: "lkm", age: "25" }
+  ];
+  user2 = [
+    { name: "xyz", age: "24" },
+    { name: "abc", age: "23" },
+    { name: "lkm", age: "25" }
+  ];
+  constructor(public dataService: DataService) {
+    this.user = this.user1;
   }
-  printDetails(){
-   this.name=Math.random();
-   return this.dataService.count++;
+  printDetails() {
+    this.name = Math.random();
+    return this.dataService.count++;
   }
-  increasecount(){
+  increasecount() {
     this.dataService.count++;
+  }
+  order() {
+    console.log("orderclicking...");
+    this.user =
+      this.user[0].name == this.user1[0].name ? this.user2 : this.user1;
+  }
+  print(event) {
+    console.log("evnet", event.target.value);
+    this.name = event.target.value;
   }
 }
